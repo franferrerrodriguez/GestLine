13981d5d55489c74c90ff96b2c00c59a9c290300
Francisco J. Ferrer <franferrerrodriguez@gmail.com>
2021-05-26 10:20:29 +0200

    apuntamos a local

--- Frontend/src/assets/config/pro.js
+++ Frontend/src/assets/config/pro.js
@@ -1,3 +1,4 @@
 var CONFIG_PRO = {
-    gateway: 'http://ec2-15-237-150-199.eu-west-3.compute.amazonaws.com:9061'
+    //gateway: 'http://ec2-15-237-150-199.eu-west-3.compute.amazonaws.com:9061'
+    gateway: 'http://localhost:9061'
 }
